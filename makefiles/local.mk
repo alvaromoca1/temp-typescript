d3a2c4f47a960cbbd24e2c3345c7df8dd9a5088c
alvaro morales caballero <alvaromoralescaballero@MacBook-Pro-de-alvaro.local>
2024-01-20 07:25:31 -0500

    :sparkles: feat: actualizacion del loal

--- makefiles/local.mk
+++ makefiles/local.mk
@@ -1,5 +1,5 @@
 APP_DIR           = app
-IMAGE_BUILD       = node:16.17.1-alpine3.16
+IMAGE_BUILD       = node:20-alpine3.18
 FUNCTION          = base
 UID_LOCAL        ?= $(shell id -u)
 GID_LOCAL        ?= $(shell id -g)
